4a5018f975a97f5ceeb3311f265082d667291609
Eleasar@37 <langityuta549@gmail.com>
2023-03-20 18:01:34 +0800

    adding login

---
+++ login/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <div class="loginMainDiv">
+                <div class="loginContainer">
+                    <div class="logoContainer">
+                        <div class="logBox">
+                            <img src="../images/chbc-logo.png" alt="" srcset="">
+                        </div>
+                        <p>CHBC DASHBOARD LOGIN</p>
+                    </div>
+                    <form action="">
+                        <div class="loginInput">
+                            <div class="logImg"><img src="../images/mdi_user.png" alt=""></div><input type="text" value="" placeholder="username">
+                        </div>
+                        <div class="loginInput">
+                            <div class="logImg"><img src="../images/material-symbols_key.png" alt=""></div><input type="text" value="" placeholder="password">
+                        </div>
+                            <input type="submit" value="SUBMIT">
+                    </form>
+                    
+                </div>
+        </div>
+    </main>
+</body>
+</html>
